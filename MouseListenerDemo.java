12a37653260821105947d37b55e15abedfed0879
labcsm <141944315+labcsm@users.noreply.github.com>
2023-09-14 15:42:21 +0530

    Create MouseListenerDemo.java

---
+++ MouseListenerDemo.java
@@ -0,0 +1,45 @@
+//Java MouseListener Example
+import java.awt.*;  
+import java.awt.event.*;  
+import javax.swing.*;
+public class MouseListenerDemo extends JFrame implements MouseListener
+{  
+	JLabel l;  
+      MouseListenerDemo()
+      {  
+	addMouseListener(this);  
+
+	l=new JLabel();  
+	l.setBounds(20,50,120,40);  
+	add(l);  
+	setSize(600,600);  
+	setLayout(null);  
+	setVisible(true);  
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+      }  
+	public void mouseClicked(MouseEvent e) 
+	{  
+	  l.setText("Mouse Clicked");  
+	 }  
+	public void mouseEntered(MouseEvent e)
+	{  
+	  l.setText("Mouse Entered");  
+	}  
+	public void mouseExited(MouseEvent e)
+	{  
+	  l.setText("Mouse Exited");  
+	}  
+	public void mousePressed(MouseEvent e)
+	{  
+	  l.setText("Mouse Pressed");  
+	}  
+	public void mouseReleased(MouseEvent e) 
+	{  
+	  l.setText("Mouse Released");  
+	}  
+	public static void main(String[] args)
+	{  
+		new MouseListenerDemo();  
+	}  
+}  
