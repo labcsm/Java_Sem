5ca34bf70fde8ebb1a1694bdf8316cdf1835ce4e
labcsm <141944315+labcsm@users.noreply.github.com>
2023-09-14 15:39:51 +0530

    Create App.java

---
+++ App.java
@@ -0,0 +1,15 @@
+//First.java 
+import javax.swing.*; 
+import java.applet.*;  
+import java.awt.Graphics;  
+public class App extends Applet{  
+  
+public void paint(Graphics g){  
+g.drawString("welcome to applet",150,150);  
+}  
+  
+}  
+/* 
+<applet code="First1.class" width="300" height="300"> 
+</applet> 
+*/
